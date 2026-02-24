9045f79490697fa042792aec5397a46abdc01068
shritikav <shritikaverma24@gmail.com>
2026-02-24 21:08:51 +0530

    basic page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.render("index");
+});
 
-app.get('/', (req, res) => {});
 
 app.listen(3000);
